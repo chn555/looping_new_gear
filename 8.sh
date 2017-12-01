3d9965c8caff09550e0e2d4117eb9500395e9aa6
chn555 <chn566@gmail.com>
2017-12-01 22:46:46 +0200

    stuff

---
+++ 8.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+day=$(date +%a)
+
+case $day in
+	Sun)
+		echo Sunday, fuck sundays
+		;;
+	Mon)
+		echo Monday, starting to get the weekly rythem
+		;;
+	Tue)
+		echo Tuesday, halfway there
+		;;
+	Wed)
+		echo Wednesday, can you taste the weekend?
+		;;
+	Thu)
+		echo Thursday, weekend is within reach
+		;;
+	Fri)
+		echo Friday, this is the day
+		;;
+	Sun)
+		echo Sunday, cuz every good thing must end
+		;;
+
+esac
